8cbac85c9cd82dd3093f6b280e6849bbda6462e4
valnaj <najeanvalentin@gmail.com>
2024-04-23 20:55:20 -0400

    - Deep clustering test - scripts for docker app created

---
+++ app/main-v0.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+import numpy as np
+import joblib
+import pandas as pd
+
+app = FastAPI()
+model = joblib.load('xgb_model_trained.pkl')  # Ensure the model is loaded correctly
+
+class PredictionInput(BaseModel):
+    features: list  # List of lists, where each sublist is a set of features
+
+@app.post("/predict/")
+async def make_prediction(input: PredictionInput):
+    try:
+        # Create a DataFrame from the input features
+        # The column names must match exactly with those used during the model training
+        columns = columns = ["age", "job", "marital", "education", "default", "housing", "loan",
+                            "contact", "month", "day_of_week", "campaign", "pdays", "previous",
+                            "poutcome", "cons.price.idx", "cons.conf.idx", "nr.employed"]
+        data = pd.DataFrame(input.features, columns=columns)
+        
+        # Ensure the DataFrame shape matches the expected input shape for your model
+        if data.shape[1] != len(columns):
+            return {"error": f"Expected {len(columns)} features, but got {data.shape[1]}"}
+
+        # Predict using the loaded model
+        prediction = model.predict(data)
+        return {"prediction": prediction.tolist()}
+    except Exception as e:
+        return {"error": str(e)}
